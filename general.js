af1518a8629106c70190bc318dfae8e113f4fe99
uzielfischer <uzielfischer@gmail.com>
2023-04-26 22:05:35 -0600

    Se realziaron algunos cambios de estilos en el index con el color de fonde los servicios y del contacto, se cambio los efectos de color del nav de las demas secciones.

--- general.js
+++ general.js
@@ -1,10 +1,14 @@
 const header = document.querySelector(".cabecera");
+const header_otros = document.querySelector(".cabecera_otros");
 const toggleMenu = document.getElementById('menu_burger');
 const menuResponsive = document.getElementById('menu_nav');
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("abajo",window.scrollY>0)
+})
 
+window.addEventListener("scroll", function() {
+    header_otros.classList.toggle("abajo",window.scrollY>0)
 })
 
 toggleMenu.addEventListener('click', () => {
