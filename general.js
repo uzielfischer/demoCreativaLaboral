122b5cfcf0c3a2065fc4fa0d60145f7a3238af94
uzielfischer <uzielfischer@gmail.com>
2023-05-04 20:55:56 -0600

    se regreso la version af1518a8629106c70190bc318dfae8e113f4fe99

--- general.js
+++ general.js
@@ -1,14 +1,15 @@
 const header = document.querySelector(".cabecera");
-const header_otros = document.querySelector(".cabecera_otros");
 const toggleMenu = document.getElementById('menu_burger');
 const menuResponsive = document.getElementById('menu_nav');
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("abajo",window.scrollY>0)
-})
 
+<<<<<<< HEAD
 window.addEventListener("scroll",function(){
     header_otros = document.toggle("abajo",window.screenY>0)
+=======
+>>>>>>> parent of af1518a (Se realziaron algunos cambios de estilos en el index con el color de fonde los servicios y del contacto, se cambio los efectos de color del nav de las demas secciones.)
 })
 
 toggleMenu.addEventListener('click', () => {
