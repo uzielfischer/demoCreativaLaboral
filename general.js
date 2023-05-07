53369b9c5ea787b6b17c81485ee9e1f15a105395
uzielfischer <uzielfischer@gmail.com>
2023-05-06 20:27:26 -0600

    Se corrigio el bug que se tenia en la cabecera del nav y se cambio estilos sin scroll del nav

--- general.js
+++ general.js
@@ -1,16 +1,11 @@
-const header = document.querySelector(".cabecera");
-const toggleMenu = document.getElementById('menu_burger');
+const header = document.getElementById("cabecera_menu");
+const toggleMenu = document.getElementById('menu_burger')
 const menuResponsive = document.getElementById('menu_nav');
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("abajo",window.scrollY>0)
-
-<<<<<<< HEAD
-window.addEventListener("scroll",function(){
-    header_otros = document.toggle("abajo",window.screenY>0)
-=======
->>>>>>> parent of af1518a (Se realziaron algunos cambios de estilos en el index con el color de fonde los servicios y del contacto, se cambio los efectos de color del nav de las demas secciones.)
 })
+
 
 toggleMenu.addEventListener('click', () => {
     menuResponsive.classList.toggle('menu_nav--show')
